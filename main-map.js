979da7deb4b1aa9e9fe811042dd5948b6856d6cf
lisa-limousy <limousy-lisa@gmail.com>
2017-12-27 12:49:31 +0100

    external AJAX json point added

--- main-map.js
+++ main-map.js
@@ -1,53 +1,64 @@
-// The JSON data
+// The intern JSON data
 var json = [
 	{"id":1,"title":"Angkor Wat", "latitude":"13.412500", "longitude":"103.866667"},
 	{"id":2,"title":"Taj-Mahal", "latitude":"27.175", "longitude":"78.041944"}
 ];
 
-var map = [
-	{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"off"}]}
-];
-var monument = document.getElementById('footer');
-
+var map;
 
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.querySelector('#map'), {
 		zoom: 2,
 		center: new google.maps.LatLng(0.785020, 0.274128)
 	});
-	for(var i = 1; i < json.length; i++) {
-		var obj = json[i];
-		var Zone1 = new google.maps.Circle({
+	
+	map.data.loadGeoJson(
+            'https://raw.githubusercontent.com/cecilerousset/monuments-quizz/map/monuments.geo.json');
+	map.data.setStyle({
+  fillColor: '#2687bf',
+  fillOpacity: .3,
+  strokeWeight: 0
+});
+	
+	console.log(google.maps.Data.Feature.getGeometry());
+	
+      
+
+        
+	//for(var i = 1; i < json.length; i++) 
+		/*var obj = json[7];
+		var zone1 = new google.maps.Circle({
 			map: map,
             strokeWeight: 0,
             fillOpacity: 0,
 			center: new google.maps.LatLng(obj.latitude,obj.longitude),
 			radius: 9900000,
 		});
-		var Zone2 = new google.maps.Circle({
+		var zone2 = new google.maps.Circle({
 			map: map,
             strokeWeight: 0,
             fillOpacity: 0,
 			center: new google.maps.LatLng(obj.latitude,obj.longitude),
 			radius: 1500000,
 		});
-		var Zone3 = new google.maps.Circle({
+		var zone3 = new google.maps.Circle({
 			map: map,
             strokeWeight: 0,
             fillOpacity: 0,
 			center: new google.maps.LatLng(obj.latitude,obj.longitude),
 			radius: 300000,
 		});
-		Zone1.addListener('click', function() {
+		zone1.addListener('click', function(event) {
           alert("Tu es... sur le bon continent, mais bon...");
         });
-		Zone2.addListener('click', function() {
+		zone2.addListener('click', function(event) {
           alert("C'est presque ça !");
         });
-		Zone3.addListener('click', function() {
+		zone3.addListener('click', function(event) {
 			alert("Bravo, c'est ça !")
-		})
-	}
+		})*/
+	
+	
 	
 }
 
