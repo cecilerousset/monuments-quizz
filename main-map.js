027c84d0298ac3f382205e1f323efb62e7718a98
lisa-limousy <limousy-lisa@gmail.com>
2017-12-28 00:05:48 +0100

    Circle ok for every monuments

--- main-map.js
+++ main-map.js
@@ -5,47 +5,48 @@ var json = [
 ];
 
 var map;
+var monumentsName = document.querySelector("#title").innerHTML;
 
 function initMap() {
 	map = new google.maps.Map(document.querySelector('#map'), {
 		zoom: 2,
-		center: new google.maps.LatLng(0.785020, 0.274128)
+		center: {lat: 0.785020, lng: 0.274128}
 	});
+		map.data.loadGeoJson('https://raw.githubusercontent.com/cecilerousset/monuments-quizz/map/monuments.geo.json');
 	
-	map.data.loadGeoJson(
-            'https://raw.githubusercontent.com/cecilerousset/monuments-quizz/map/monuments.geo.json');
-	map.data.setStyle({
-  fillColor: '#2687bf',
-  fillOpacity: .3,
-  strokeWeight: 0
-});
-	
-	console.log(google.maps.Data.Feature.getGeometry());
-	
-      
+	map.data.setStyle(function(feature) {
+    	return {
+			icon:feature.getProperty('icon')
+		};
+  });
+	  
+  google.maps.event.addListener(map.data,'addfeature',function(e){
+          
+	  	var circleCenter = new google.maps.LatLng();
+          circleCenter = e.feature.getGeometry().get();
+	  	  console.log(e.feature.getProperty("title"));
 
-        
-	//for(var i = 1; i < json.length; i++) 
-		/*var obj = json[7];
-		var zone1 = new google.maps.Circle({
+
+	  	  //if(e.feature.getProperty("title")== monumentsName){
+			  var zone1 = new google.maps.Circle({
 			map: map,
             strokeWeight: 0,
             fillOpacity: 0,
-			center: new google.maps.LatLng(obj.latitude,obj.longitude),
+			center: circleCenter,
 			radius: 9900000,
 		});
 		var zone2 = new google.maps.Circle({
 			map: map,
             strokeWeight: 0,
             fillOpacity: 0,
-			center: new google.maps.LatLng(obj.latitude,obj.longitude),
+			center: circleCenter,
 			radius: 1500000,
 		});
 		var zone3 = new google.maps.Circle({
 			map: map,
-            strokeWeight: 0,
-            fillOpacity: 0,
-			center: new google.maps.LatLng(obj.latitude,obj.longitude),
+            strokeWeight: 1,
+            fillOpacity: 5,
+			center: circleCenter,
 			radius: 300000,
 		});
 		zone1.addListener('click', function(event) {
@@ -56,18 +57,19 @@ function initMap() {
         });
 		zone3.addListener('click', function(event) {
 			alert("Bravo, c'est ça !")
-		})*/
-	
-	
-	
+		})
+		 //}
+ 
+  });
+   
 }
+      
 
-// Load GeoJSON.
-//map.data.loadGeoJson('google.json');
+        
+	//for(var i = 1; i < json.length; i++) 
+	//var obj = json[7];
 	
 /*
-Utiliser AJAX pour aller chercher mon JSon
-Ou alors le faire avec les pays
 Webdesign map
 GeoJson countries : https://github.com/johan/world.geo.json/tree/master/countries
 Data event layer : https://developers.google.com/maps/documentation/javascript/examples/layer-data-event
