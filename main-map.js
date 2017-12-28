73611f331e8794e5e73a052c92480e27654fe4b0
lisa-limousy <limousy-lisa@gmail.com>
2017-12-28 02:32:56 +0100

    Code bien indenté

--- main-map.js
+++ main-map.js
@@ -6,75 +6,67 @@ function initMap() {
 		zoom: 2,
 		center: {lat: 0.785020, lng: 0.274128}
 	});
-		map.data.loadGeoJson('https://raw.githubusercontent.com/cecilerousset/monuments-quizz/map/monuments.geo.json');
-	  
+		
 	
-  google.maps.event.addListener(map.data,'addfeature',function(e){
+	map.data.loadGeoJson('https://raw.githubusercontent.com/cecilerousset/monuments-quizz/map/monuments.geo.json');
+	  
+	google.maps.event.addListener(map.data,'addfeature',function(e) {
          
-	  
-	  	var circleCenter = new google.maps.LatLng();
-          circleCenter = e.feature.getGeometry().get();
-	 
-	  if(monumentsName == e.feature.getProperty('title')){
-		  var zone1 = new google.maps.Circle({
-			map: map,
-            strokeWeight: 0,
-            fillOpacity: 0,
-			center: circleCenter,
-			radius: 9900000,
-		});
-		var zone2 = new google.maps.Circle({
-			map: map,
-            strokeWeight: 0,
-            fillOpacity: 0,
-			center: circleCenter,
-			radius: 1500000,
-		});
-		var zone3 = new google.maps.Circle({
-			map: map,
-            strokeWeight: 0,
-            fillOpacity: 0,
-			center: circleCenter,
-			radius: 300000,
-		});
-		zone1.addListener('click', function(event) {
-          alert("Tu es... sur le bon continent, mais bon...");
-        });
-		zone2.addListener('click', function(event) {
-          alert("C'est presque ça !");
-        });
-		zone3.addListener('click', function(event) {
-			alert("Bravo, c'est ça !")
-		})
-	  }
-	  
- 	/*e.feature.getGeometry().forEach(function(path){
-          
-             path.forEach(function(latLng){
-				 console.log("Je suis là");
-			 })
-          
-          });*/
-	  
-	  	  //if(e.feature.getProperty("title")== monumentsName){
-
-
-   
-})
-
+		var circleCenter = new google.maps.LatLng();
+		circleCenter = e.feature.getGeometry().get();
+		
+		if(monumentsName == e.feature.getProperty('title')){
+			var zone1 = new google.maps.Circle({
+				map: map,
+            	strokeWeight: 0,
+            	fillOpacity: 0,
+				center: circleCenter,
+				radius: 100000000,
+			});	
+			
+		  	var zone2 = new google.maps.Circle({
+				map: map,
+            	strokeWeight: 0,
+            	fillOpacity: 0,
+				center: circleCenter,
+				radius: 3500000,
+			});
+			
+			var zone3 = new google.maps.Circle({
+				map: map,
+            	strokeWeight: 0,
+            	fillOpacity: 0,
+				center: circleCenter,
+				radius: 1000000,
+			});
+			
+			var zone4 = new google.maps.Circle({
+				map: map,
+            	strokeWeight: 0,
+            	fillOpacity: 0.1,
+				center: circleCenter,
+				radius: 250000,
+			});
+			
+			zone1.addListener('click', function(event) {
+				alert("Tu en es très loin... désolé...");
+			});
+			zone2.addListener('click', function(event) {
+				alert("Tu es peut-être sur le bon continent, ou c'est celui d'à côté !");
+        	});
+			zone3.addListener('click', function(event) {
+				alert("C'est presque ça !");
+        	});
+			zone4.addListener('click', function(event) {
+				alert("Bravo, c'est ça !")
+			});
+		}
+	})
 }
-      
 
-        
-	//for(var i = 1; i < json.length; i++) 
-	//var obj = json[7];
 /* 
-
-1°) foreach marker si title == name alors on affiche tout le bordel
-http://jsfiddle.net/doktormolle/qtDR6/
-2°) enlever ces putains de markers, opacity ou icon
-
-/*
+enlever markers, opacity ou icon
+ou trouver un moyen de ne pas les faire apparaitre automatiquement
 
 Data event layer : https://developers.google.com/maps/documentation/javascript/examples/layer-data-event
 
