4335a148a840712b4fa52c5b68763e05b1481a7b
lisa-limousy <limousy-lisa@gmail.com>
2018-01-06 17:07:52 +0100

    Final project

--- main-map.js
+++ main-map.js
@@ -1,27 +1,82 @@
 var map;
-function initMap(){
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 2,
-        center: new google.maps.LatLng(2.8,-187.3),
-        mapTypeId: 'terrain'
-    });
+var getDataName = location.search.substring(1, location.search.length);
+var dataName = getDataName.replace(/%20/gi, " ");
+var monumentsName = document.querySelector("#monument");
+monumentsName.innerHTML = dataName;
+var monumentsPicture = document.querySelector("#monument-picture");
+var comments = document.querySelector("#text-comments");
+var gauge = document.querySelector("#gauge");
 
-    var script = document.createElement('script');
-    // This example uses a local copy of the GeoJSON stored at
-    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-// Loop through the results array and place a marker for each
-// set of coordinates.
-window.eqfeed_callback = function(results) {
-    for (var i = 0; i < results.features.length; i++) {
-        var coords = results.features[i].geometry.coordinates;
-        var latLng = new google.maps.LatLng(coords[1],coords[0]);
-        var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-        });
-    }
+function initMap() {
+	map = new google.maps.Map(document.querySelector('#map'), {
+		zoom: 3,
+		center: {lat: 48.932091, lng:  2.087123}
+	});
+		
+	map.data.loadGeoJson('https://raw.githubusercontent.com/cecilerousset/monuments-quizz/map/monuments.geo.json');
+	
+	map.data.setStyle(function(feature) {
+    	return {visible:false};
+  	});
+	  
+	google.maps.event.addListener(map.data,'addfeature',function(e) {
+         
+		var circleCenter = new google.maps.LatLng();
+		circleCenter = e.feature.getGeometry().get();
+	
+		if(monumentsName.innerHTML == e.feature.getProperty('title')){
+			monumentsPicture.src = e.feature.getProperty('img');
+			var zone1 = new google.maps.Circle({
+				map: map,
+            	strokeWeight: 0,
+            	fillOpacity: 0,
+				center: circleCenter,
+				radius: 100000000,
+			});	
+			
+		  	var zone2 = new google.maps.Circle({
+				map: map,
+            	strokeWeight: 0,
+            	fillOpacity: 0,
+				center: circleCenter,
+				radius: 3500000,
+			});
+			
+			var zone3 = new google.maps.Circle({
+				map: map,
+            	strokeWeight: 0,
+            	fillOpacity: 0,
+				center: circleCenter,
+				radius: 1000000,
+			});
+			
+			var zone4 = new google.maps.Circle({
+				map: map,
+            	strokeWeight: 0,
+            	fillOpacity: 0,
+				center: circleCenter,
+				radius: 250000,
+			});
+			
+			zone1.addListener('click', function(event) {
+				comments.innerHTML = "You're far far away...";
+				gauge.src = "img/pointer/location-pointer1.svg";
+			});
+			zone2.addListener('click', function(event) {
+				comments.innerHTML = "You're on the correct continent or it's the next one !";
+				gauge.src = "img/pointer/location-pointer2.svg";
+        	});
+			zone3.addListener('click', function(event) {
+				comments.innerHTML = "You're almost there !";
+				gauge.src = "img/pointer/location-pointer3.svg";
+        	});
+			zone4.addListener('click', function(event) {
+				comments.innerHTML = "Congratulations, you're right !"
+				gauge.src = "img/pointer/location-pointer4.svg";
+				map.setOptions({
+					
+				});
+			});
+		}
+	})
 }
--- main.js
+++ main.js
@@ -1,15 +1,120 @@
+//-------------NOMBRES ALEATOIRES POUR IMAGES--------------
+var randomnumber1 = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+var randomnumber2 = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+var randomnumber3 = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
 
-var randomnumber = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
-
+//------------------LISTE DES MONUMENTS--------------------
 var JSONdata = {
-	"angkor" : "img/angkor-vat1.jpg",
-    "big-ben" : "img/big-ben.jpg",
-    "colisee" : "img/colisée-rome.jpg",
-    "empire-state-building" : "img/empire-state-building.jpg",
-    "muraille" : "img/grande-muraille-chine.jpg",
-    "machu-pichu" : "img/Machu-Picchu.jpg",
-    "opera-sydney" : "img/opera-sydney.jpg",
-    "pyramide-gizeh" : "img/pyramide-gizeh.jpg",
-    "taj-mahal" : "img/taj-mahal.jpg",
-    "tour-eiffel" : "img/tour-eiffel.jpg",
+	"0" : "img/angkor-vat1.jpg",
+    "1" : "img/big-ben.jpg",
+    "2" : "img/colisée-rome.jpg",
+    "3" : "img/empire-state-building.jpg",
+    "4" : "img/grande-muraille-chine.jpg",
+    "5" : "img/Machu-Picchu.jpg",
+    "6" : "img/opera-sydney.jpg",
+    "7" : "img/pyramide-gizeh.jpg",
+    "8" : "img/taj-mahal.jpg",
+    "9" : "img/tour-eiffel.jpg",
 };
+
+//-----------------AVOIR MONUMENTS DIFFERENTS---------------
+while(randomnumber2==randomnumber1){
+    randomnumber2 = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+}
+
+while(randomnumber3==randomnumber2 || randomnumber3==randomnumber1){
+    randomnumber3 = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+}
+
+var img1 = JSONdata[randomnumber1];
+var img2 = JSONdata[randomnumber2];
+var img3 = JSONdata[randomnumber3];
+
+//-------------REMPLACE LES IMAGES DANS HTML----------------
+var test1 = document.querySelector('#monuments .picture0');
+test1.src = img1;
+var test1 = document.querySelector('#monuments .picture1');
+test1.src = img2;
+var test1 = document.querySelector('#monuments .picture2');
+test1.src = img3;
+
+//------------------------TITRE-----------------------------
+var JSONtitle = {
+    "0" : "Angkor Wat",
+    "1" : "Big Ben",
+    "2" : "Colosseum",
+    "3" : "Empire State Building",
+    "4" : "Great Wall of China",
+    "5" : "Machu Picchu",
+    "6" : "Sydney Opera House",
+    "7" : "Giza pyramids",
+    "8" : "Taj Mahal",
+    "9" : "Eiffel Tower",
+};
+
+var randomnumber4 = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+
+var question;
+
+if (randomnumber4 == 1){
+    question = JSONtitle[randomnumber1];
+}
+if (randomnumber4 == 2){
+    question = JSONtitle[randomnumber2];
+}
+if (randomnumber4 == 3){
+    question = JSONtitle[randomnumber3];
+}
+
+//-----------------------QUESTION---------------------------
+
+document.querySelector('#name').innerHTML = question;
+
+//-----------------------CLIQUER----------------------------
+var modal = document.querySelector('.modal-content');
+var num1 = document.querySelector('.picture0');
+var num2 = document.querySelector('.picture1');
+var num3 = document.querySelector('.picture2');
+
+
+num1.addEventListener('click', function(e){
+    if(randomnumber4 == 1){
+	    modal.classList.add("modal-content-visible");
+    }
+});
+
+num2.addEventListener('click', function(e){
+    if(randomnumber4 == 2){
+        modal.classList.add("modal-content-visible");
+    };
+});
+
+num3.addEventListener('click', function(e){
+    if(randomnumber4 == 3){
+        modal.classList.add("modal-content-visible");
+    };
+});
+
+//---------------------BOUTON----------------------
+var bouton = document.querySelector('#inputSubmit');
+var display = document.querySelector('.modal-content2');
+
+bouton.addEventListener('click', function(e){
+    e.preventDefault();
+    display.classList.add("modal-content-invisible");
+});
+
+//----------------------ENTER NAME--------------------------
+var playerName = document.querySelector('.player');
+var input = document.querySelector('#inputName');
+
+function write(e) {
+    if (e.target.value == "") {
+        playerName.innerHTML = "Default";
+    }
+    else {
+        playerName.innerHTML = e.target.value;
+    }
+}
+
+input.addEventListener('keyup', write);
